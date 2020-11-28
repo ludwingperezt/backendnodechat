e850a925fc541a6bcb84726447600191df8a1624
Ludwing Perez <ludwing@ludwing-laptop>
2020-11-27 18:34:29 -0600

    Uso inicial de router.

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const router = express.Router();
 
 const app = express();
 
-app.use('/', function(req, res) {
-  res.send('Hola!');
+app.use(router);
+
+router.get('/message', function(req, res) {
+  res.send('Hola desde GET');
 });
+
+router.post('/message', function(req, res) {
+  res.send('Hola desde POST');
+});
+
 
 app.listen(3000);
 
