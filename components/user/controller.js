91e6eb7de40eb31cc4f53b47f9ac1cc8e5c9f2c7
Ludwing Perez <ludwing@ludwing-laptop>
2020-12-07 20:24:51 -0600

    Agregar componentes de usuarios.

---
+++ components/user/controller.js
@@ -0,0 +1,22 @@
+const store = require('./store');
+
+function addUser(name) {
+  if (!name) {
+    return Promise.reject('Invalid name');
+  }
+
+  const user = {
+    name,
+  };
+
+  return store.add(user)
+}
+
+function getUsers() {
+  return store.list();
+}
+
+module.exports = {
+  addUser,
+  getUsers
+}
