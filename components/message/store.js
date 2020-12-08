879caf71ad885e1e387403a3725dbbacb17c81c9
Ludwing Perez <ludwing@ludwing-laptop>
2020-12-07 20:49:04 -0600

    Agregar referencia a usuario en modelo de mensajes.

--- components/message/store.js
+++ components/message/store.js
@@ -6,14 +6,23 @@ function addMessage(message) {
 }
 
 async function getMessages(filterUser) {
-  let filter = {}
+  return new Promise((resolve, reject) => {
+    let filter = {}
 
-  if (filterUser !== null) {
-    filter = {user: filterUser};
-  }
+    if (filterUser !== null) {
+      filter = {user: filterUser};
+    }
+    
+    Model.find(filter)
+      .populate('user')
+      .exec((error, populated) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(populated);
+      });
+  });
   
-  const messages = await Model.find(filter);
-  return messages;
 }
 
 async function updateText(id, message) {
