d24416405d7f826f39b6c14e988ff87fd7ddb879
Ludwing Perez <ludwing@ludwing-laptop>
2020-12-09 08:09:41 -0600

    Implementacion de websockets para comunicacion en tiempo real.

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+
 const bodyParser = require('body-parser');
+const socket = require('./socket');
 const db = require('./db');
 const router = require('./network/routes');
 
@@ -7,16 +11,18 @@ const url = `mongodb://admin1:admin1@localhost:27017/chatdb`;
 
 db(url);
 
-const app = express();
 
 app.use(bodyParser.json());  // bodyParser siempre debe ir antes de router
 app.use(bodyParser.urlencoded({extended: false}));
+
+// Iniciar el socket
+socket.connect(server);
 
 router(app);
 
 // levantar el servidor de estáticos de express
 app.use('/app', express.static('public'));
 
-app.listen(3000);
-
-console.log('La aplicación está escuchando en http://localhost:3000');
+server.listen(3000, function(){
+  console.log('La aplicación está escuchando en http://localhost:3000');
+});
