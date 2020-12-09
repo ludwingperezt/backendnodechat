68b3d2a14d070682139d780a54d4ef312cb829dc
Ludwing Perez <ludwing@ludwing-laptop>
2020-12-08 18:28:39 -0600

    Configuracion para subir archivos y guardar la referencia en base de datos.

--- components/message/controller.js
+++ components/message/controller.js
@@ -1,17 +1,24 @@
 const store = require('./store');
 
-function addMessage(chat, user, message) {
+function addMessage(chat, user, message, file) {
   return new Promise((resolve, reject) => {
     if (!chat || !user || !message) {
       console.error('[messageController] No hay usuario o mensaje');
       return reject('Los datos son incorrectos');
     }
 
+    let fileUrl = '';
+
+    if (file) {
+      fileUrl = 'http://localhost:3000/app/files/' + file.filename;
+    }
+
     const fullMessage = {
       chat,
       user,
       message,
-      date: new Date()
+      date: new Date(),
+      file: fileUrl
     };
     
     store.add(fullMessage);
